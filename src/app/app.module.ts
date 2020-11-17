39915628c53cb9ae30ab663384c6eb77eb9110a6
theawesomedaniel <daniel4go@gmail.com>
2020-11-17 14:49:13 -0300

    ultima versao

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+ import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAnalyticsModule,
+    AngularFirestoreModule
+  ],
+  declarations: [ AppComponent ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase : { 
+      apiKey: "AIzaSyDBr3Gs3dWsRLxKxdD6uqkzxVyncPvmHVg",
+  authDomain: "angfire-1d9ac.firebaseapp.com",
+  databaseURL: "https://angfire-1d9ac.firebaseio.com",
+  projectId: "angfire-1d9ac",
+  storageBucket: "angfire-1d9ac.appspot.com",
+  messagingSenderId: "27625352273",
+  appId: "1:27625352273:web:744df30d8f4ad511169c70",
+  measurementId: "G-RLR05GSP0J"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
